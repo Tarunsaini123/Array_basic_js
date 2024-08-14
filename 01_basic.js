d0b4f90e8bd03a04b636daa3af81d8ed6d292c1b
Tarun saini <tarunsaini75130@gmail.com>
2024-08-14 12:59:00 +0530

    first commit

---
+++ 01_basic.js
@@ -0,0 +1,77 @@
+const newArr = [0,1,2,3,4,5,6,7,8]
+
+const newArr1 =new Array(9,8,7,6,5,4)
+
+// console.log(newArr1);
+// console.log(newArr1);
+
+// newArr.push(7)
+// newArr.pop()
+
+// newArr.unshift(55)
+// newArr.shift()
+
+// newArr.indexOf(3)
+// console.log(newArr.indexOf(3));
+
+
+
+// console.log(newArr);
+
+
+
+// slice, splice
+
+// console.log ("A", newArr);
+
+// const myNewArr = newArr.slice(1,3)
+// console.log(myNewArr);
+// console.log(newArr)
+
+
+// console.log ("B", newArr);
+// const myNewArr2 = newArr.splice(1,3)
+// console.log(myNewArr2);
+
+// console.log(newArr)
+
+
+
+// concate and spread operator
+
+marvalHeros = ["ironman","superman","Hindustan"]
+
+coun_name = ["India","Pakistan","Nepal0","Bhutan"]
+
+
+// const allMerge = marvalHeros.concat(coun_name)
+// console.log(allMerge);
+
+  //another method to merge two or more array
+
+// const mergeToAll =  [...marvalHeros, ...coun_name]
+// console.log(mergeToAll);
+ 
+
+
+console.log(Array.isArray("Tarun"));
+console.log(Array.from("Tarun"));
+console.log(Array.from({name:"Tarun"}));
+
+
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1,score2,score3));
+
+
+
+
+
+
+
+
+
+
